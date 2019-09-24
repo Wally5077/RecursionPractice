884a75fdf485d9428b851b140bb8ba48264e806c
華 <wally55077@gmail.com>
2019-09-24 23:46:01 +0800

    RecursionPractice 1

--- src/RecursionPractice.java
+++ src/RecursionPractice.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 
 public class RecursionPractice {
 
-    
+    private static int Fibonacci(int index) {
+        return (index > 1) ? Fibonacci(index - 1) + Fibonacci(index - 2) : index;
+    }
+
+    private static int factorial(int index) {
+        return (index > 1) ? index * factorial(index - 1) : 1;
+    }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Start");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(factorial(i) + " ");
+        }
     }
 }
